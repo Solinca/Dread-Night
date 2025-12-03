41c7d957f02f4d67b5054c0521c97360becaa2a0
Yann Grallan <grallanyann84@gmail.com>
2025-12-03 12:05:03 +0100

    Setup base item gameplay tag

--- Source/DreadNight/Private/Item/Data/ItemGameplayTag.cpp
+++ Source/DreadNight/Private/Item/Data/ItemGameplayTag.cpp
@@ -3,4 +3,8 @@
 
 #include "DreadNight/Public/Item/Data/ItemGameplayTag.h"
 
- 
+UE_DEFINE_GAMEPLAY_TAG(Item, "Item")
+UE_DEFINE_GAMEPLAY_TAG(Item_Weapon, "Item.Weapon")
+UE_DEFINE_GAMEPLAY_TAG(Item_Armor, "Item.Armor")
+UE_DEFINE_GAMEPLAY_TAG(Item_Food, "Item.Food")
+UE_DEFINE_GAMEPLAY_TAG(Item_Drink, "Item.Drink")
