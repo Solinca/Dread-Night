7d33c6d6c7dcb565684ac1a647e403c7814ad494
Xima <74795630+XimaLc@users.noreply.github.com>
2025-12-03 17:18:18 +0100

    Add buttons + AddDynamic

--- Source/DreadNight/Private/UserWidgets/CraftStationWidget.cpp
+++ Source/DreadNight/Private/UserWidgets/CraftStationWidget.cpp
@@ -1,3 +1,23 @@
 #include "UserWidgets/CraftStationWidget.h"
 
 #include "Components/TextBlock.h"
+#include "Components/Button.h"
+
+void UCraftStationWidget::NativeConstruct()
+{
+	if (CraftButton)
+		CraftButton->OnClicked.AddDynamic(this, &UCraftStationWidget::OnCraftButtonClicked);
+
+	if (QuitButton)
+		QuitButton->OnClicked.AddDynamic(this, &UCraftStationWidget::OnQuitButtonClicked);
+}
+
+void UCraftStationWidget::OnCraftButtonClicked()
+{
+
+}
+
+void UCraftStationWidget::OnQuitButtonClicked()
+{
+
+}
--- Source/DreadNight/Public/UserWidgets/CraftStationWidget.h
+++ Source/DreadNight/Public/UserWidgets/CraftStationWidget.h
@@ -5,13 +5,29 @@
 #include "CraftStationWidget.generated.h"
 
 class UTextBlock;
+class UButton;
 
 UCLASS()
 class DREADNIGHT_API UCraftStationWidget : public UUserWidget
 {
 	GENERATED_BODY()
-	
+
+	virtual void NativeConstruct() override;
+
 public:
 	UPROPERTY(meta=(BindWidget))
 	TObjectPtr<UTextBlock> StationName;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> CraftButton;
+
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UButton> QuitButton;
+
+protected:
+	UFUNCTION()
+	void OnCraftButtonClicked();
+
+	UFUNCTION()
+	void OnQuitButtonClicked();
 };
