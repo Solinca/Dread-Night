0e8cdc3fe1ee1f2eb4fdd4b9d82012c45513c8da
Beud61 <baptiste.beudet@gmail.com>
2025-12-03 14:45:27 +0100

    création de sword combat component

---
+++ Source/DreadNight/Public/Components/SwordCombatComponent.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SwordCombatComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DREADNIGHT_API USwordCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+public:	
+	USwordCombatComponent();
+
+protected:
+	virtual void BeginPlay() override;
+
+	FTimerHandle AttackCooldownTimerHandle;
+
+	bool bCanAttack;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackCooldown = 0.5f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackRange = 150.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackRadius = 80.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackDamage = 25.0f;
+
+	void PerformAttackTrace();
+	void ResetAttack();
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	void Attack();
+		
+};
