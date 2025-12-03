300e6f96ca213ab17572b98e44e93dac8a1ce39b
Yann Grallan <grallanyann84@gmail.com>
2025-12-03 12:05:03 +0100

    Setup base item gameplay tag

--- Source/DreadNight/Public/Item/Data/ItemGameplayTag.h
+++ Source/DreadNight/Public/Item/Data/ItemGameplayTag.h
@@ -3,5 +3,10 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "NativeGameplayTags.h"
 
- 
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Item)
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Item_Weapon)
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Item_Armor)
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Item_Food)
+UE_DECLARE_GAMEPLAY_TAG_EXTERN(Item_Drink)
