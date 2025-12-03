14bbd100a29e902040ebb23f0178748316d33932
Charles Lesage <charles19lesage06@gmail.com>
2025-12-03 14:04:42 +0100

    Add Warrior AI character and controller classes
    Introduces AWarriorAICharacter and AWarriorAIController classes with basic constructors and BeginPlay overrides. Includes corresponding Unreal Engine asset files for character and controller setup.

---
+++ Source/DreadNight/Private/IA/Characters/WarriorAICharacter.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IA/Characters/WarriorAICharacter.h"
+
+
+// Sets default values
+AWarriorAICharacter::AWarriorAICharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWarriorAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
