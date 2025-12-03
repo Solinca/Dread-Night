e1a063f85ed3fe1041623d11ccaa248243d11097
LucaN64 <luca.niro9@gmail.com>
2025-12-03 16:54:41 +0100

    Ajouts fonctionnalités sur bp

--- Source/DreadNight/Public/UserWidgets/OptionsWidget.h
+++ Source/DreadNight/Public/UserWidgets/OptionsWidget.h
@@ -2,6 +2,8 @@
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
+#include "Components/Textblock.h"
+#include "Components/Button.h"
 #include "OptionsWidget.generated.h"
 
 UCLASS()
@@ -13,4 +15,7 @@ protected:
 	virtual void NativePreConstruct() override;
 	virtual void NativeConstruct() override;
 	virtual void NativeTick(const FGeometry& Geometry, float DeltaTime) override;
+
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TBTitle = nullptr;
 };
