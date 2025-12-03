66ea182611a9446135826d7f84041af3536703b9
Charles Lesage <charles19lesage06@gmail.com>
2025-12-03 13:51:54 +0100

    Add base AI character and controller classes
    Introduces ABaseAICharacter and ABaseAIController with initial logic for AI possession and behavior tree execution. Updates build dependencies to include AIModule, GameplayTags, and GameplayTasks. Adds and modifies AI testing level maps for development and testing purposes.

---
+++ Source/DreadNight/Private/IA/Characters/BaseAICharacter.cpp
@@ -0,0 +1,21 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IA/Characters/BaseAICharacter.h"
+
+
+// Sets default values
+ABaseAICharacter::ABaseAICharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	AutoPossessAI = EAutoPossessAI::PlacedInWorldOrSpawned;
+}
+
+// Called when the game starts or when spawned
+void ABaseAICharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
