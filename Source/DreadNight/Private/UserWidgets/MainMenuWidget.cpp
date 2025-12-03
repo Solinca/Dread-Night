fb980071aed56b414405e274b6d18a4e90fb6d1f
LucaN64 <luca.niro9@gmail.com>
2025-12-03 14:18:46 +0100

    Ajout affichage sur écran + fonctions menu

--- Source/DreadNight/Private/UserWidgets/MainMenuWidget.cpp
+++ Source/DreadNight/Private/UserWidgets/MainMenuWidget.cpp
@@ -1,4 +1,6 @@
 #include "UserWidgets/MainMenuWidget.h"
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetSystemLibrary.h"
 
 void UMainMenuWidget::NativePreConstruct()
 {
@@ -13,4 +15,25 @@ void UMainMenuWidget::NativeConstruct()
 void UMainMenuWidget::NativeTick(const FGeometry& Geometry, float DeltaTime)
 {
 	Super::NativeTick(Geometry, DeltaTime);
+}
+
+void UMainMenuWidget::ActionContinue()
+{
+
+}
+
+void UMainMenuWidget::ActionNewGame()
+{
+
+}
+
+void UMainMenuWidget::ActionOptions()
+{
+
+}
+
+void UMainMenuWidget::ActionQuit()
+{
+	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
+	UKismetSystemLibrary::QuitGame(GetWorld(), PC, EQuitPreference::Quit, true);
 }
