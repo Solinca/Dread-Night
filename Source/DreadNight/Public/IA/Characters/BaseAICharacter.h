1636691484e68fc563eb12286e4549d841e71b98
Charles Lesage <charles19lesage06@gmail.com>
2025-12-03 16:10:50 +0100

    Add team system for AI characters and controllers
    Introduces EGameTeam enum and TeamFunctionLibrary to manage team assignments and queries for AI characters and controllers. Implements IGenericTeamAgentInterface in BaseAICharacter and adds team attitude logic to BaseAIController. This enables team-based behavior and perception for AI agents.

--- Source/DreadNight/Public/IA/Characters/BaseAICharacter.h
+++ Source/DreadNight/Public/IA/Characters/BaseAICharacter.h
@@ -3,11 +3,12 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GenericTeamAgentInterface.h"
 #include "GameFramework/Character.h"
 #include "BaseAICharacter.generated.h"
 
 UCLASS(Abstract, BlueprintType, Blueprintable, Category = "AI", ClassGroup = "AIController")
-class DREADNIGHT_API ABaseAICharacter : public ACharacter
+class DREADNIGHT_API ABaseAICharacter : public ACharacter, public IGenericTeamAgentInterface
 {
 	GENERATED_BODY()
 
@@ -15,6 +16,10 @@ public:
 	// Sets default values for this character's properties
 	ABaseAICharacter();
 
+	virtual void SetGenericTeamId(const FGenericTeamId& TeamID) override;
+	virtual FGenericTeamId GetGenericTeamId() const override;
+
+	virtual ETeamAttitude::Type GetTeamAttitudeTowards(const AActor& Other) const override;
 protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
---
+++ Source/DreadNight/Public/Team/TeamType.h
@@ -0,0 +1,14 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+UENUM(BlueprintType, Blueprintable)
+enum EGameTeam : uint8
+{
+	None = 0 UMETA(Hidden),
+	Enemy	 UMETA(DisplayName = "Enemy"),
+	Player	 UMETA(DisplayName = "Player"),
+	Other	 UMETA(DisplayName = "Other")
+};
