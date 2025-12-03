3772cb1dec003a4b1f6db6a9fb9755d0bfec01bb
Yann Grallan <grallanyann84@gmail.com>
2025-12-03 14:24:38 +0100

    Add UItemInstance class for item stack management
    Introduces UItemInstance with logic for stacking, using, and managing item instances. Includes methods for stack operations, depletion handling, and event broadcasting to support item inventory functionality.

---
+++ Source/DreadNight/Private/Items/Object/ItemInstance.cpp
@@ -0,0 +1,90 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Items/Object/ItemInstance.h"
+
+#include "Items/Data/ItemDataAsset.h"
+
+bool UItemInstance::TryStealInstance(UItemInstance* Other)
+{
+	if (!Other || !ItemData)
+		return false;
+
+	if (CanBeStackedWith(Other, EStackMethod::Fully))
+	{
+		TryAdd(Other->StackNumber);
+		Other->TryUse(Other->StackNumber);		 
+		return true;
+	}
+	if (CanBeStackedWith(Other, EStackMethod::Partially))
+	{
+		const int InstanceToAdd = StackNumber - ItemData->StackLimit;		
+		TryAdd(InstanceToAdd);
+		Other->TryUse(InstanceToAdd);	
+		return true;		
+	}
+
+	return false;
+
+}
+
+void UItemInstance::DestroyIfEmpty()
+{
+	if (StackNumber <= 0)
+	{
+		OnItemDepleted.Broadcast(this);
+		
+		MarkAsGarbage();
+	}
+}
+
+bool UItemInstance::CanBeStackedWith(const UItemInstance* Other, EStackMethod StackMethod) const
+{
+	if (!Other || !ItemData)
+		return false;
+
+	const bool bIsSameType = Other->ItemData->Type.MatchesTagExact(ItemData->Type);
+	
+	switch (StackMethod) {
+	case EStackMethod::Fully: 
+		return bIsSameType && StackNumber + Other->StackNumber <= ItemData->StackLimit; 
+	case EStackMethod::Partially: 
+		return bIsSameType && StackNumber < ItemData->StackLimit; 
+	case EStackMethod::SameType:
+		return bIsSameType; 
+	}
+	
+ 
+	return false;
+}
+
+ 
+
+bool UItemInstance::TryStackWith(UItemInstance* Other)
+{
+	if (!Other || !ItemData)
+		return false;
+
+	return TryStealInstance(Other);
+}
+
+bool UItemInstance::TryAdd(const int NumberOfInstanceToAdd)
+{
+	if (StackNumber + NumberOfInstanceToAdd <= ItemData->StackLimit)
+		return false;
+
+	StackNumber += NumberOfInstanceToAdd;
+	OnItemStackChange.Broadcast(this, StackNumber); 
+	return true;
+}
+
+bool UItemInstance::TryUse(const int NumberOfInstanceToUse)
+{
+	if (NumberOfInstanceToUse > StackNumber)
+		return false;
+
+	StackNumber -= NumberOfInstanceToUse;
+	OnItemStackChange.Broadcast(this, StackNumber);
+	DestroyIfEmpty();
+	return true;
+}
---
+++ Source/DreadNight/Public/Items/Object/ItemInstance.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ItemInstance.generated.h"
+
+class UItemDataAsset;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FEmptyItemEvent,class UItemInstance*, ItemInstance);
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_TwoParams(FItemInstanceStackChangeEvent,class UItemInstance*, ItemInstance, int, NewInstanceNumber);
+/**
+ * 
+ */
+UCLASS()
+class DREADNIGHT_API UItemInstance : public UObject
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintReadOnly)
+	TObjectPtr<UItemDataAsset> ItemData;
+
+	UPROPERTY(BlueprintReadOnly)
+	int StackNumber;
+
+	bool TryStealInstance(UItemInstance* Other);
+
+	void DestroyIfEmpty();
+public:
+	
+	enum class EStackMethod
+	{
+		Fully,
+		Partially,
+		SameType
+	};
+ 
+	bool CanBeStackedWith(const UItemInstance* Other, EStackMethod StackMethod) const;
+
+	bool TryStackWith(UItemInstance* Other);
+
+	bool TryUse(const int NumberOfInstanceToUse);
+
+	bool TryAdd(const int NumberOfInstanceToAdd);
+	
+	FEmptyItemEvent OnItemDepleted;
+	FItemInstanceStackChangeEvent OnItemStackChange; 
+};
