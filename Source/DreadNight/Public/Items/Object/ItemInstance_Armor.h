69bfce2b526ec898b53fa4f42c1172b30e6f71b5
Yann Grallan <grallanyann84@gmail.com>
2025-12-03 14:39:38 +0100

    Add derived from ItemInstance to add specialization

---
+++ Source/DreadNight/Public/Items/Object/ItemInstance_Armor.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ItemInstance.h"
+#include "ItemInstance_Armor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREADNIGHT_API UItemInstance_Armor : public UItemInstance
+{
+	GENERATED_BODY()
+};
