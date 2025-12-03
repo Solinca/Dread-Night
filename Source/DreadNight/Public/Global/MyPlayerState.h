9e805f9d51a8f408788124b3115fbb366696a1af
Nicolas Corbi <n.corbi@GA.local>
2025-12-03 13:44:35 +0100

    STRUCT-2 STRUCT-3 - Create a GameModeBase, PlayerState and GameState

---
+++ Source/DreadNight/Public/Global/MyPlayerState.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerState.h"
+#include "MyPlayerState.generated.h"
+
+UCLASS()
+class DREADNIGHT_API AMyPlayerState : public APlayerState
+{
+	GENERATED_BODY()
+	
+};
