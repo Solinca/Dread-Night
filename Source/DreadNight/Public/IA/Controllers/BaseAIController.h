84ea06dec88177ef19b35db6936e875d60b8b598
Charles Lesage <charles19lesage06@gmail.com>
2025-12-03 14:26:49 +0100

    Add AI perception and stimulus handling to BaseAIController
    Introduces UAIPerceptionComponent to BaseAIController and sets up event bindings for perception updates. Adds callback methods for sight and hearing stimuli, enabling Blueprint integration and internal routing of perception events.

--- Source/DreadNight/Public/IA/Controllers/BaseAIController.h
+++ Source/DreadNight/Public/IA/Controllers/BaseAIController.h
@@ -3,6 +3,7 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "Perception/AIPerceptionComponent.h"
 #include "Runtime/AIModule/Classes/AIController.h"
 #include "BaseAIController.generated.h"
 
@@ -34,5 +35,38 @@ protected:
 	UFUNCTION(BlueprintNativeEvent, DisplayName = "SetupBlackboard")
 	void BP_SetupBlackboard(UBlackboardComponent* BlackboardComponent);
 
-	virtual void SetupBlackboard(UBlackboardComponent* BlackboardComponent) {};
+	virtual void SetupBlackboard(UBlackboardComponent* BlackboardComponent) {}
+
+#pragma region AI Perception Base Callback
+	
+	UFUNCTION()
+	virtual void OnPerceptionUpdated(const TArray<AActor*>& UpdatedActors) {}
+
+	UFUNCTION()
+	virtual void OnTargetPerceptionInfoUpdated(const FActorPerceptionUpdateInfo& UpdateInfo) {}
+	
+	virtual void OnTargetPerceptionUpdated(AActor* Actor, FAIStimulus& Stimulus) {}
+
+	UFUNCTION()
+	virtual void OnTargetPerceptionForgotten(AActor* Actor) {}
+	
+#pragma endregion // AI Perception Base Callback
+
+#pragma region Senses Callback
+	
+	UFUNCTION(BlueprintNativeEvent, DisplayName = "OnSightStimulusUpdated")
+	void BP_OnSightStimulusUpdated(AActor* Actor, FAIStimulus& Stimulus, const ETeamAttitude::Type TeamAttitude);
+
+	virtual void OnSightStimulusUpdated(AActor* Actor, FAIStimulus& Stimulus, const ETeamAttitude::Type TeamAttitude) {}
+
+	UFUNCTION(BlueprintNativeEvent, DisplayName = "OnHearingStimulusUpdated")
+	void BP_OnHearingStimulusUpdated(AActor* Actor, FAIStimulus& Stimulus, const ETeamAttitude::Type TeamAttitude);
+
+	virtual void OnHearingStimulusUpdated(AActor* Actor, FAIStimulus& Stimulus, const ETeamAttitude::Type TeamAttitude) {}
+	
+#pragma endregion // Sense Callback
+	
+private:
+	UFUNCTION()
+	void InternalOnTargetPerceptionUpdated(AActor* Actor, FAIStimulus Stimulus);
 };
