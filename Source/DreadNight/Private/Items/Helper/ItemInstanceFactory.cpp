d24919d25a8f6ff76c42e0182eafd02b8d74f3e3
Yann Grallan <grallanyann84@gmail.com>
2025-12-04 09:01:20 +0100

    Fix item instance creation logic with else-if chain
    Replaced multiple if statements with else-if to ensure only one item instance type is created based on the first matching tag. This prevents multiple assignments to NewItem when an item matches more than one tag.

--- Source/DreadNight/Private/Items/Helper/ItemInstanceFactory.cpp
+++ Source/DreadNight/Private/Items/Helper/ItemInstanceFactory.cpp
@@ -16,15 +16,15 @@ UItemInstance* FItemInstanceFactory::CreateItem(UItemDataAsset* ItemDataAsset, c
 	UItemInstance* NewItem = nullptr;
 	if (ItemDataAsset->Type.MatchesTag(GT_Item_Weapon))
 		NewItem = NewObject<UItemInstance_Weapon>();
-	if (ItemDataAsset->Type.MatchesTag(GT_Item_Armor))
+	else if (ItemDataAsset->Type.MatchesTag(GT_Item_Armor))
 		NewItem = NewObject<UItemInstance_Armor>();
-	if (ItemDataAsset->Type.MatchesTag(GT_Item_Food))
+	else if (ItemDataAsset->Type.MatchesTag(GT_Item_Food))
 		NewItem = NewObject<UItemInstance_Food>();
-	if (ItemDataAsset->Type.MatchesTag(GT_Item_Drink))
+	else if (ItemDataAsset->Type.MatchesTag(GT_Item_Drink))
 		NewItem = NewObject<UItemInstance_Drink>();
 
 	
-	if (ItemDataAsset->Type.MatchesTag(GT_Item))
+	else if (ItemDataAsset->Type.MatchesTag(GT_Item))
 		NewItem = NewObject<UItemInstance>();
 	
 	if (!NewItem)
