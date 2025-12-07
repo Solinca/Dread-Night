ace8b1c263634bb97d4c61fbc6361aceb43673cb
Yann Grallan <116149587+Xanhos@users.noreply.github.com>
2025-12-07 18:45:21 +0100

    STRUCT-11 - Create a usable interface for item

---
+++ Source/DreadNight/Private/Items/Interface/UsableItem.cpp
@@ -0,0 +1,4 @@
+﻿#include "Items/Interface/UsableItem.h"
+
+
+// Add default functionality here for any IUsableItem functions that are not pure virtual.
--- Source/DreadNight/Public/Items/Object/ItemInstance_Drink.h
+++ Source/DreadNight/Public/Items/Object/ItemInstance_Drink.h
@@ -2,12 +2,18 @@
 
 #include "CoreMinimal.h"
 #include "ItemInstance.h"
+#include "Items/Interface/UsableItem.h"
 #include "ItemInstance_Drink.generated.h"
 
 
 
 UCLASS()
-class DREADNIGHT_API UItemInstance_Drink : public UItemInstance
+class DREADNIGHT_API UItemInstance_Drink : public UItemInstance, public IUsableItem
 {
+public:
+	virtual FName GetActionName() override;
+	virtual void Use(AActor* Player) override;
+	
+private:
 	GENERATED_BODY()
 };
