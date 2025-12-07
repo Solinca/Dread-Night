a173f06ed32eb688d3a4c7c8d85a47709eb98b9e
Yann Grallan <116149587+Xanhos@users.noreply.github.com>
2025-12-07 18:28:33 +0100

    STRUCT-1 & GP-13 - Create Save System and ActorReference Struct
    Co-authored-by: Nicolas Corbi <corbi.nicolas@gmail.com>

---
+++ Source/DreadNight/Private/SaveSystem/DN_SaveGame.cpp
@@ -0,0 +1,129 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SaveSystem/DN_SaveGame.h"
+
+#include "EngineUtils.h"
+#include "Kismet/GameplayStatics.h"
+#include "SaveSystem/SavableActor.h"
+#include "Serialization/ObjectAndNameAsStringProxyArchive.h"
+
+
+void UDN_SaveGame::CollectSaveData(UWorld* WorldContext)
+{
+    for (TActorIterator<AActor> It(WorldContext); It; ++It)
+	{
+		if (!It->Implements<USavableActor>())
+		{
+			continue;
+		}
+
+    	FSaveDataStruct ActorSaveInfo;
+
+    	ISavableActor* SavableObject = Cast<ISavableActor>(*It);
+    	
+    	ActorSaveInfo.bIsDynamicActor = SavableObject->IsDynamicallySpawned();
+    	ActorSaveInfo.SpawnTransform = SavableObject->GetSpawnTransform();
+    	ActorSaveInfo.Identifier = SavableObject->GetUniqueIdentifier();		
+    	if (ActorSaveInfo.bIsDynamicActor)
+    	{
+    		ActorSaveInfo.ActorClass = SavableObject->GetSpawnClass();
+    	}
+		
+    	
+    	FMemoryWriter MemoryWriter(ActorSaveInfo.Data, true);
+    	FObjectAndNameAsStringProxyArchive Ar(MemoryWriter, false);
+    	It->Serialize(Ar);
+
+    	GameSaveData.GameData.Add(ActorSaveInfo);
+	}
+}
+
+TMap<FName, AActor*> UDN_SaveGame::BuildWorldActorCache(UWorld* WorldContext) const
+{
+	TMap<FName, AActor*> ActorCache;
+	for (TActorIterator<AActor> It(WorldContext); It; ++It)
+	{
+		if (!It->Implements<USavableActor>())
+		{
+			continue;
+		}
+
+		ISavableActor* Object = Cast<ISavableActor>(*It);
+		if (!Object->IsDynamicallySpawned())
+		{
+			ActorCache.Add(Object->GetUniqueIdentifier(), *It);
+		}
+	}
+
+	return MoveTemp(ActorCache);	
+}
+
+TMap<FName, ISavableActor*> UDN_SaveGame::BuildWorldSavableCache(UWorld* WorldContext) const
+{
+	TMap<FName, ISavableActor*> SavableCache;
+	for (TActorIterator<AActor> It(WorldContext); It; ++It)
+	{
+		if (!It->Implements<USavableActor>())
+		{
+			continue;
+		}
+
+		ISavableActor* Object = Cast<ISavableActor>(*It);
+		SavableCache.Add(Object->GetUniqueIdentifier(), Object); 
+	}
+
+	return MoveTemp(SavableCache);	
+}
+
+void UDN_SaveGame::GatherAllSaveData(UWorld* WorldContext)
+{
+	CollectSaveData(WorldContext);
+	
+}
+
+void UDN_SaveGame::UseAllSaveData(UWorld* WorldContext)
+{
+	TMap<FName, AActor*> ActorCache = BuildWorldActorCache(WorldContext);
+	for (FSaveDataStruct& SaveActorData : GameSaveData.GameData)
+	{
+		AActor* TargetActor = nullptr;
+		if (!SaveActorData.bIsDynamicActor && ActorCache.Contains(SaveActorData.Identifier))
+		{
+			TargetActor = ActorCache[SaveActorData.Identifier];
+			TargetActor->SetActorTransform(SaveActorData.SpawnTransform);
+		}
+		else
+		{
+			if (!SaveActorData.ActorClass) continue;
+            
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.SpawnCollisionHandlingOverride = 
+				ESpawnActorCollisionHandlingMethod::AlwaysSpawn;
+            
+			TargetActor = WorldContext->SpawnActor<AActor>(
+				SaveActorData.ActorClass,
+				SaveActorData.SpawnTransform,
+				SpawnParams
+			);
+			if (ISavableActor* SavableActor = Cast<ISavableActor>(TargetActor))
+			{
+				FGuid ActorGuid;
+				FGuid::Parse(SaveActorData.Identifier.ToString(),ActorGuid);
+				SavableActor->SetIsDynamicallySpawned(SaveActorData.ActorClass, ActorGuid);
+			}
+		}
+
+		if (TargetActor && TargetActor->Implements<USavableActor>())
+		{ 
+			FMemoryReader MemoryReader(SaveActorData.Data, true);
+			FObjectAndNameAsStringProxyArchive Ar(MemoryReader, false);
+			TargetActor->Serialize(Ar); 
+		}
+	}
+	TMap<FName, ISavableActor*> FinalSavableCache = BuildWorldSavableCache(WorldContext);
+	for (TPair<FName, ISavableActor*>& Savable : FinalSavableCache)
+	{
+		Savable.Value->OnPostLoad(FinalSavableCache);
+	}
+}
