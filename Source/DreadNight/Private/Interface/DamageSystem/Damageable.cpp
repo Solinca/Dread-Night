00c48a1fbd5e2762d047bb56ca9d85d163c03cd2
Yann Grallan <grallanyann84@gmail.com>
2025-12-08 09:38:26 +0100

    Clean up Damageable.cpp includes and comments
    Removed unnecessary comments and reordered the include statement in Damageable.cpp for better clarity and code hygiene.

--- Source/DreadNight/Private/Interface/DamageSystem/Damageable.cpp
+++ Source/DreadNight/Private/Interface/DamageSystem/Damageable.cpp
@@ -1,10 +1,6 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "DamageSystem/Interface/Damageable.h"
 
-
-#include "DamageSystem/Interface/Damageable.h"
-
-
-// Add default functionality here for any IDamageable functions that are not pure virtual.
+ 
 bool IDamageable::TryApplyDamage(float Damage, AActor* DamageInstigator)
 {
 	return false;
