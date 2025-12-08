8841ce4a8bfab055280987dac45aea197df89b0c
Beud61 <116149729+Beud61@users.noreply.github.com>
2025-12-08 20:52:39 +0100

    GP-11.1 - Create a sword combat component

---
+++ Source/DreadNight/Public/Components/SwordCombatComponent.h
@@ -0,0 +1,46 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/ActorComponent.h"
+#include "SwordCombatComponent.generated.h"
+
+class UBoxComponent;
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class DREADNIGHT_API USwordCombatComponent : public UActorComponent
+{
+	GENERATED_BODY()
+
+protected:	
+	USwordCombatComponent();
+	virtual void BeginPlay() override;
+
+	FTimerHandle AttackCooldownTimerHandle;
+
+	bool bCanAttack;
+	bool bIsAttacking;
+
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackCooldown = 1.f;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float AttackDamage = 25.0f;
+	UPROPERTY()
+	TWeakObjectPtr<UBoxComponent> SwordHitBox;
+	UPROPERTY()
+	TArray<AActor*> HitActors;
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Combat")
+	float CollisionEnableTime = 0.3f;
+
+	void EnableSwordCollision();
+	void DisableSwordCollision();
+	void ResetAttack();
+
+	UFUNCTION()
+	void OnSwordOverlap(UPrimitiveComponent* OverlappedComp, AActor* OtherActor,
+		UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+
+public:
+	UFUNCTION(BlueprintCallable, Category = "Combat")
+	void Attack();
+	void SetHitBoxComponent(UBoxComponent* HitBox);
+};
