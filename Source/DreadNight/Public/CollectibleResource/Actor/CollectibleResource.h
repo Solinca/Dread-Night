9bb0360f8af35c90d0e1affec9d88f35173d0299
Yann Grallan <116149587+Xanhos@users.noreply.github.com>
2025-12-08 20:54:59 +0100

    GP-3 - Create a class collectible resource

---
+++ Source/DreadNight/Public/CollectibleResource/Actor/CollectibleResource.h
@@ -0,0 +1,41 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "DamageSystem/Interface/Damageable.h"
+#include "GameFramework/Actor.h"
+#include "CollectibleResource.generated.h"
+
+class UItemDataAsset;
+
+UCLASS()
+class DREADNIGHT_API ACollectibleResource : public AActor, public IDamageable
+{
+public:
+	virtual bool TryApplyDamage(float Damage, AActor* DamageInstigator) override;
+
+private:
+	GENERATED_BODY()
+
+public: 
+	ACollectibleResource();
+
+protected: 
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	const TObjectPtr<UItemDataAsset> ItemDataAsset;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector2D ItemQuantityRange;
+
+	int CurrentItemQuantity;
+
+	UPROPERTY(EditDefaultsOnly)
+	TObjectPtr<UStaticMeshComponent> ResourceMesh;
+	
+public:
+
+	
+};
