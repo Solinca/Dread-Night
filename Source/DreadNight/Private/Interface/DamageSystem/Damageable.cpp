31f23c58500b782e1fb7d4ac8a454a2175568e8a
Yann Grallan <grallanyann84@gmail.com>
2025-12-08 09:37:58 +0100

    Add Damageable interface and default implementation
    Introduces the IDamageable interface and its Unreal UInterface wrapper, providing a virtual TryApplyDamage method with a default implementation. This sets up the foundation for damage handling in the project.

---
+++ Source/DreadNight/Private/Interface/DamageSystem/Damageable.cpp
@@ -0,0 +1,11 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DamageSystem/Interface/Damageable.h"
+
+
+// Add default functionality here for any IDamageable functions that are not pure virtual.
+bool IDamageable::TryApplyDamage(float Damage, AActor* DamageInstigator)
+{
+	return false;
+}
---
+++ Source/DreadNight/Public/DamageSystem/Interface/Damageable.h
@@ -0,0 +1,20 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Interface.h"
+#include "Damageable.generated.h"
+ 
+UINTERFACE()
+class UDamageable : public UInterface
+{
+	GENERATED_BODY()
+};
+ 
+class DREADNIGHT_API IDamageable
+{
+	GENERATED_BODY()
+ 
+public:
+
+	virtual bool TryApplyDamage(float Damage,AActor* DamageInstigator);
+};
