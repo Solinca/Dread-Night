779fdae2a59dfd3257337286551fa272f8c12db9
Yann Grallan <grallanyann84@gmail.com>
2025-12-08 09:29:02 +0100

    Add CollectibleResource actor class
    Introduces the ACollectibleResource actor with configurable item data and quantity range. Initializes item quantity randomly within the specified range on BeginPlay.

---
+++ Source/DreadNight/Private/Actor/CollectibleResource/CollectibleResource.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actor/CollectibleResource/CollectibleResource.h"
+
+ 
+ACollectibleResource::ACollectibleResource()
+{ 
+	PrimaryActorTick.bCanEverTick = false;
+}
+ 
+void ACollectibleResource::BeginPlay()
+{
+	Super::BeginPlay();
+
+	CurrentItemQuantity = FMath::RandRange(ItemQuantityRange.X,ItemQuantityRange.Y);
+}
+ 
+
---
+++ Source/DreadNight/Public/Actor/CollectibleResource/CollectibleResource.h
@@ -0,0 +1,33 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "CollectibleResource.generated.h"
+
+class UItemDataAsset;
+
+UCLASS()
+class DREADNIGHT_API ACollectibleResource : public AActor
+{
+	GENERATED_BODY()
+
+public: 
+	ACollectibleResource();
+
+protected: 
+	virtual void BeginPlay() override;
+
+	UPROPERTY(EditDefaultsOnly)
+	const TObjectPtr<UItemDataAsset> ItemDataAsset;
+
+	UPROPERTY(EditDefaultsOnly)
+	FVector2D ItemQuantityRange;
+
+	int CurrentItemQuantity; 
+	
+public:
+
+	
+};
