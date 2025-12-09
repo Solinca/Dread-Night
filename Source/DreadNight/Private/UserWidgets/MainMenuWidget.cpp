565526ddeaa239f60b86552a4b3140a5256bf4e3
LucaN64 <luca.niro9@gmail.com>
2025-12-09 08:51:33 +0100

    Passage aux events sur MainMenuWidget

--- Source/DreadNight/Private/UserWidgets/MainMenuWidget.cpp
+++ Source/DreadNight/Private/UserWidgets/MainMenuWidget.cpp
@@ -1,33 +1,36 @@
 #include "UserWidgets/MainMenuWidget.h"
 #include "Kismet/GameplayStatics.h"
 #include "Kismet/KismetSystemLibrary.h"
+#include "UserWidgets/OptionsWidget.h"
 
 void UMainMenuWidget::NativeConstruct()
 {
 	Super::NativeConstruct();
 
-	ButtonContinue->OnClicked.AddDynamic(this, &UMainMenuWidget::ActionContinue);
-	ButtonNewGame->OnClicked.AddDynamic(this, &UMainMenuWidget::ActionNewGame);
-	ButtonOptions->OnClicked.AddDynamic(this, &UMainMenuWidget::ActionOptions);
-	ButtonQuit->OnClicked.AddDynamic(this, &UMainMenuWidget::ActionQuit);
+	ButtonContinue->OnClicked.AddDynamic(this, &UMainMenuWidget::OnContinueClicked);
+	ButtonNewGame->OnClicked.AddDynamic(this, &UMainMenuWidget::OnNewGameClicked);
+	ButtonOptions->OnClicked.AddDynamic(this, &UMainMenuWidget::OnOptionsClicked);
+	ButtonQuit->OnClicked.AddDynamic(this, &UMainMenuWidget::OnQuitClicked);
 }
 
-void UMainMenuWidget::ActionContinue()
+void UMainMenuWidget::OnContinueClicked()
 {
 
 }
 
-void UMainMenuWidget::ActionNewGame()
+void UMainMenuWidget::OnNewGameClicked()
 {
 
 }
 
-void UMainMenuWidget::ActionOptions()
+void UMainMenuWidget::OnOptionsClicked()
 {
-
+	RemoveFromParent();
+	UOptionsWidget* OW = CreateWidget<UOptionsWidget>(GetWorld(), OptionsWidgetClass);
+	if (OW) OW->AddToViewport();
 }
 
-void UMainMenuWidget::ActionQuit()
+void UMainMenuWidget::OnQuitClicked()
 {
 	APlayerController* PC = UGameplayStatics::GetPlayerController(GetWorld(), 0);
 	UKismetSystemLibrary::QuitGame(GetWorld(), PC, EQuitPreference::Quit, true);
