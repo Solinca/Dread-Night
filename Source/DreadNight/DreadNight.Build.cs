2a274fcdcf838b2caac413102788c0568ff08b7d
Yann Grallan <116149587+Xanhos@users.noreply.github.com>
2025-12-09 10:32:47 +0100

    GP-4 - Generate resources procedurally

--- Source/DreadNight/DreadNight.Build.cs
+++ Source/DreadNight/DreadNight.Build.cs
@@ -8,7 +8,16 @@ public class DreadNight : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "AIModule", "GameplayTags", "GameplayTasks", "UMG" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", 
+			"CoreUObject", 
+			"Engine",
+			"InputCore",
+			"EnhancedInput", 
+			"AIModule", 
+			"GameplayTags", 
+			"GameplayTasks", 
+			"UMG"
+		});
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
