3a5e98e493da74b17652d9939ad6c37fce3fb37f
LucaN64 <luca.niro9@gmail.com>
2025-12-09 12:14:05 +0100

    Ajout de la classe de base + un bp actor de test

---
+++ Source/DreadNight/Private/Actors/Building.cpp
@@ -0,0 +1,39 @@
+#include "Actors/Building.h"
+#include "Components/SceneComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+ABuilding::ABuilding()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	RootComp = CreateDefaultSubobject<USceneComponent>("RootComp");
+	RootComponent = RootComp;
+
+	MeshComp = CreateDefaultSubobject<UStaticMeshComponent>("Mesh");
+	MeshComp->SetupAttachment(RootComponent);
+	
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	MeshComp->SetCollisionResponseToAllChannels(ECR_Overlap);
+}
+
+bool ABuilding::CheckValidPlacement()
+{
+	TArray<AActor*> OverlapingActors;
+	MeshComp->GetOverlappingActors(OverlapingActors);
+
+	return OverlapingActors.Num() == 0;
+}
+
+void ABuilding::PlaceBuilding()
+{
+	if (bIsPlaced || !CheckValidPlacement()) return;
+
+	bIsPlaced = true;
+	MeshComp->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	MeshComp->SetCollisionResponseToAllChannels(ECR_Block);
+}
+
+void ABuilding::DestroyBuilding()
+{
+	Destroy();
+}
---
+++ Source/DreadNight/Public/Actors/Building.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "Building.generated.h"
+
+class USceneComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class DREADNIGHT_API ABuilding : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	USceneComponent* RootComp;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	UStaticMeshComponent* MeshComp;
+
+	bool bIsPlaced = false;
+
+public:
+	ABuilding();
+	bool CheckValidPlacement();
+	virtual void PlaceBuilding();
+	virtual void DestroyBuilding();
+};
