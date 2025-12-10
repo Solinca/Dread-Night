994a8d91f1839be61f9c6db64cd2fed61a4d4d6b
LucaN64 <luca.niro9@gmail.com>
2025-12-10 10:57:07 +0100

    UI-2 - Options-Menu (#82)

---
+++ Source/DreadNight/Private/UserWidgets/OptionsWidget.cpp
@@ -0,0 +1,124 @@
+#include "UserWidgets/OptionsWidget.h"
+#include "Kismet/KismetSystemLibrary.h"
+#include "GameFramework/GameUserSettings.h"
+#include "UserWidgets/MainMenuWidget.h"
+
+void UOptionsWidget::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (ButtonReturn) ButtonReturn->OnClicked.AddDynamic(this, &UOptionsWidget::OnReturnClicked);
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+	Settings->LoadSettings();
+
+	if (ComboBoxWindowMode)
+	{
+		SetupComboBox<EWindowMode::Type>(
+			ComboBoxWindowMode,
+			WindowModeMap,
+			Settings->GetFullscreenMode()
+		);
+
+		ComboBoxWindowMode->OnSelectionChanged.AddDynamic(this, &UOptionsWidget::OnWindowModeChanged);
+	}
+
+	if (ComboBoxResolution)
+	{
+		SetupComboBox<FIntPoint>(
+			ComboBoxResolution,
+			ResolutionMap,
+			Settings->GetScreenResolution()
+		);
+
+		ComboBoxResolution->OnSelectionChanged.AddDynamic(this, &UOptionsWidget::OnResolutionChanged);
+	}
+
+	if (ComboBoxGraphics)
+	{
+		SetupComboBox<int>(
+			ComboBoxGraphics,
+			GraphicsMap,
+			Settings->GetOverallScalabilityLevel()
+		);
+
+		ComboBoxGraphics->OnSelectionChanged.AddDynamic(this, &UOptionsWidget::OnGraphicsChanged);
+	}
+
+	if (CheckBoxVSync)
+	{
+		CheckBoxVSync->SetIsChecked(Settings->bUseVSync);
+		CheckBoxVSync->OnCheckStateChanged.AddDynamic(this, &UOptionsWidget::OnCheckboxVSyncChanged);
+	}
+}
+
+void UOptionsWidget::OnWindowModeChanged(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	if (!WindowModeMap.Contains(SelectedItem)) return;
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+
+	Settings->SetFullscreenMode(WindowModeMap[SelectedItem]);
+	Settings->ApplySettings(false);
+}
+
+void UOptionsWidget::OnResolutionChanged(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	if (!ResolutionMap.Contains(SelectedItem)) return;
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+
+	Settings->SetScreenResolution(ResolutionMap[SelectedItem]);
+	Settings->ApplySettings(false);
+}
+
+void UOptionsWidget::OnGraphicsChanged(FString SelectedItem, ESelectInfo::Type SelectionType)
+{
+	if (!GraphicsMap.Contains(SelectedItem)) return;
+
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+
+	Settings->SetOverallScalabilityLevel(GraphicsMap[SelectedItem]);
+	Settings->ApplySettings(false);
+}
+
+void UOptionsWidget::OnCheckboxVSyncChanged(bool bIsChecked)
+{
+	UGameUserSettings* Settings = GEngine->GetGameUserSettings();
+	Settings->bUseVSync = bIsChecked;
+	Settings->ApplySettings(false);
+}
+
+void UOptionsWidget::OnReturnClicked()
+{
+	RemoveFromParent();
+
+	if (UMainMenuWidget* MMW = CreateWidget<UMainMenuWidget>(GetWorld(), MainMenuWidgetClass))
+	{
+		MMW->AddToViewport();
+	}
+}
+
+UOptionsWidget::UOptionsWidget(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer)
+{
+	WindowModeMap = {
+		{TEXT("Fullscreen"), EWindowMode::Fullscreen},
+		{TEXT("Windowed Fullscreen"), EWindowMode::WindowedFullscreen},
+		{TEXT("Windowed"), EWindowMode::Windowed}
+	};
+
+	TArray<FIntPoint> Array;
+	UKismetSystemLibrary::GetSupportedFullscreenResolutions(Array);
+	for (auto& i : Array)
+	{
+		ResolutionMap.Add(FString::Printf(TEXT("%dx%d"), i.X, i.Y), i);
+	}
+
+	GraphicsMap = {
+		{TEXT("Low"), 0},
+		{TEXT("Medium"), 1},
+		{TEXT("High"), 2},
+		{TEXT("Epic"), 3},
+		{TEXT("Cinematic"), 4},
+	};
+}
---
+++ Source/DreadNight/Public/UserWidgets/OptionsWidget.h
@@ -0,0 +1,85 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Components/TextBlock.h"
+#include "Components/Button.h"
+#include "Components/ComboBoxString.h"
+#include "Components/CheckBox.h"
+#include "OptionsWidget.generated.h"
+
+class UMainMenuWidget;
+
+UCLASS()
+class DREADNIGHT_API UOptionsWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+private:
+	TMap<FString, EWindowMode::Type> WindowModeMap;
+	TMap<FString, FIntPoint> ResolutionMap;
+	TMap<FString, int> GraphicsMap;
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UTextBlock> TBTitle = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* ComboBoxWindowMode = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* ComboBoxResolution = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	UComboBoxString* ComboBoxGraphics = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	UCheckBox* CheckBoxVSync = nullptr;
+
+	UPROPERTY(meta = (BindWidget))
+	UButton* ButtonReturn = nullptr;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite)
+	TSubclassOf<UMainMenuWidget> MainMenuWidgetClass;
+
+	virtual void NativeConstruct() override;
+
+	template<typename TValue>
+	void SetupComboBox(
+		UComboBoxString* ComboBox,
+		TMap<FString, TValue>& Map,
+		const TValue& CurrentValue) {
+		if (!ComboBox) return;
+
+		ComboBox->ClearOptions();
+
+		for (const auto& Pair : Map) ComboBox->AddOption(Pair.Key);
+
+		for (const auto& Pair : Map)
+		{
+			if (Pair.Value == CurrentValue)
+			{
+				ComboBox->SetSelectedOption(Pair.Key);
+				break;
+			}
+		}
+	};
+
+	UFUNCTION()
+	void OnWindowModeChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void OnResolutionChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void OnGraphicsChanged(FString SelectedItem, ESelectInfo::Type SelectionType);
+
+	UFUNCTION()
+	void OnCheckboxVSyncChanged(bool bIsChecked);
+
+	UFUNCTION()
+	void OnReturnClicked();
+
+public:
+	UOptionsWidget(const FObjectInitializer& ObjectInitializer);
+};
