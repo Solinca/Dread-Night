5c3c40dd4a8d300263b66c759420fa5fddcca9a8
Charles Lesage <charles19lesage06@gmail.com>
2025-12-10 15:25:39 +0100

    Add trap collision and ticking logic to Trap actor
    Introduces a TrapCollisionComponent with a custom collision profile for traps, and implements ticking logic with timer and overlap handling in the Trap actor. The trap now supports configurable ticking behavior, overlap events for AI characters, and improved lifecycle management. Updates DefaultEngine.ini with new collision profiles and redirects.
    I need to later add IA Pawn collision channel in the Trap collision profil.

--- Source/DreadNight/Private/Actors/Traps/Trap.cpp
+++ Source/DreadNight/Private/Actors/Traps/Trap.cpp
@@ -3,17 +3,81 @@
 
 #include "Actors/Traps/Trap.h"
 
-
-// Sets default values
 ATrap::ATrap()
 {
-	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
+
+	TrapCollisionComponent = CreateDefaultSubobject<UBoxComponent>("Trap Collision Component");
+	TrapCollisionComponent->SetCollisionProfileName("Trap");
 }
 
-// Called when the game starts or when spawned
+void ATrap::Tick(float DeltaSeconds)
+{
+	Super::Tick(DeltaSeconds);
+	
+	OnTrapTick();
+
+	
+}
+
 void ATrap::BeginPlay()
 {
 	Super::BeginPlay();
+
+	TrapCollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ATrap::OnBeginOverlap);
+}
+
+void ATrap::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	GetWorldTimerManager().ClearTimer(TickTimerHandle);
+}
+
+void ATrap::StartTrapTicking()
+{
+	SetActorTickEnabled(true);
 	
+	if (!bTrapIsUsingTimer || bTrapTickInfinitely)
+	{
+		return;
+	}
+	
+	GetWorldTimerManager().SetTimer(TickTimerHandle, [this]
+	{
+		StopTrapTicking();
+	}, TrapTimer, false);
+}
+
+void ATrap::StopTrapTicking()
+{
+	OnTrapTickFinish();
+	SetActorTickEnabled(false);
+}
+
+void ATrap::BP_OnTrapTrigger_Implementation(ABaseAICharacter* TrapTriggerInvestigator)
+{
+	OnTrapTrigger(TrapTriggerInvestigator);
+}
+
+void ATrap::CheckToFinishTrapTicking()
+{
+	if (bTrapIsUsingTimer || bTrapTickInfinitely)
+	{
+		return;
+	}
+	
+	if (++TrapTickCurrentCount >= TrapTickCount)
+	{
+		StopTrapTicking();
+	}
+}
+
+void ATrap::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+                           UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ABaseAICharacter* BaseAICharacter{Cast<ABaseAICharacter>(OtherActor)})
+	{
+		BP_OnTrapTrigger(BaseAICharacter);
+	}
 }
--- Source/DreadNight/Public/Actors/Traps/Trap.h
+++ Source/DreadNight/Public/Actors/Traps/Trap.h
@@ -2,16 +2,75 @@
 
 #include "CoreMinimal.h"
 #include "Actors/Building.h"
+#include "Components/BoxComponent.h"
+#include "IA/Characters/BaseAICharacter.h"
 #include "Trap.generated.h"
 
 UCLASS()
 class DREADNIGHT_API ATrap : public ABuilding
 {
 	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UBoxComponent> TrapCollisionComponent;
+	
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable", meta = (EditCondition = "!bTrapIsUsingTimer && !bTrapTickInfinitely"))
+	int32 TrapTickCount{0};
+	int32 TrapTickCurrentCount{0};
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable", meta = (EditCondition = "bTrapIsUsingTimer && !bTrapTickInfinitely"))
+	float TrapTimer{0.f};
+
+private:
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variable", meta = (AllowPrivateAccess = true))
+	bool bTrapTickInfinitely{false};
+
+	/**
+	 * Determines the trap's ticking behavior:
+	 * - false: The trap ticks at regular intervals defined by TickInterval (e.g., every second) for a set number of iterations, 
+	 *   then automatically stops by calling StopTrapTicking.
+	 * - true: The trap ticks every frame for a duration specified by TrapTimer, then automatically stops by calling StopTrapTicking.
+	 */
+	UPROPERTY(EditAnywhere, BlueprintReadOnly, Category = "Variable", meta = (EditCondition = "!bTrapTickInfinitely", AllowPrivateAccess = true))
+	bool bTrapIsUsingTimer{false};
+	
+	FTimerHandle TickTimerHandle;
 	
 public:
 	ATrap();
 
+	virtual void Tick(float DeltaSeconds) override;
+	
 protected:
 	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	/**
+	 * This function is used to start the ticking process of the trap.
+	 * After this function is called OnTrapTick will start to be called.
+	 */
+	void StartTrapTicking();
+
+	/**
+	 * This function is used to stop the ticking process of the trap.
+	 * When this function is called OnTrapTickFinish will be called.
+	 */
+	void StopTrapTicking();
+
+	virtual void OnTrapTick() {}
+
+	virtual void OnTrapTickFinish() {}
+	
+	UFUNCTION(BlueprintNativeEvent, DisplayName = "OnTrapTrigger")
+	void BP_OnTrapTrigger(ABaseAICharacter* TrapTriggerInvestigator);
+
+	virtual void OnTrapTrigger(ABaseAICharacter* TrapTriggerInvestigator) {}
+private:
+	void CheckToFinishTrapTicking();
+	
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
 };
