acfa804a5949e7c6f6c6ea2f3c7b734fa14972c3
Yann Grallan <grallanyann84@gmail.com>
2025-12-10 11:43:56 +0100

    Add ExcludeZone actor and related assets for PCG
    Introduced AExcludeZone actor class with a blocking box component to define exclusion zones for procedural content generation. Added corresponding Blueprint (BP_ExcludePCGZone), updated BaseLevel and PCG_Forest assets to integrate the new exclusion zone functionality, and made minor updates to BP_BuildingTest.

---
+++ Source/DreadNight/Private/PCG/ExcludeZone/MyClass.cpp
@@ -0,0 +1,12 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PCG/ExcludeZone/MyClass.h"
+
+AExcludeZone::AExcludeZone()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	BlockingZone = CreateDefaultSubobject<UBoxComponent>("Exclude Zone");
+	RootComponent = BlockingZone;	
+}
---
+++ Source/DreadNight/Public/PCG/ExcludeZone/MyClass.h
@@ -0,0 +1,23 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/BoxComponent.h"
+#include "UObject/Object.h"
+#include "MyClass.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREADNIGHT_API AExcludeZone : public AActor
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere)
+	TObjectPtr<UBoxComponent> BlockingZone;
+
+	AExcludeZone();
+};
