b824e4b4fee29b3c82df33baf986fca200a72b37
Yann Grallan <grallanyann84@gmail.com>
2025-12-10 11:16:47 +0100

    Update resource collection logic in TryApplyDamage
    Refactored TryApplyDamage to calculate collected resource using FMath::Min and log collection details. Also decrements CurrentItemQuantity and destroys the actor when depleted.

--- Source/DreadNight/Private/CollectibleResource/Actor/CollectibleResource.cpp
+++ Source/DreadNight/Private/CollectibleResource/Actor/CollectibleResource.cpp
@@ -3,21 +3,27 @@
 
 #include "CollectibleResource/Actor/CollectibleResource.h"
 
+#include "Items/Data/ItemDataAsset.h"
 #include "Items/Helper/ItemInstanceFactory.h"
 #include "Items/Object/ItemInstance.h"
 
 
 bool ACollectibleResource::TryApplyDamage(float Damage, AActor* DamageInstigator)
 {
+	const int ResourceCollected = FMath::Min(Damage, CurrentItemQuantity);
+	
 	//TODO Add cast to player and add inventory logic
 	if (DamageInstigator)
 	{
-		UItemInstance* ItemInstance = FItemInstanceFactory::CreateItem(ItemDataAsset, FMath::Min(Damage, CurrentItemQuantity));
-			
+		
+		UItemInstance* ItemInstance = FItemInstanceFactory::CreateItem(ItemDataAsset, ResourceCollected);
 
 		return true;
 	}
+	
+	UE_LOG(LogTemp, Log, TEXT("Resource type = %s, collected %d"), *ItemDataAsset->Type.GetTagLeafName().ToString(), ResourceCollected);
 
+	CurrentItemQuantity -= ResourceCollected;
 	if (CurrentItemQuantity <= 0)
 	{
 		Destroy();
