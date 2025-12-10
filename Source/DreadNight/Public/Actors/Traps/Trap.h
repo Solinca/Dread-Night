ec561d3cd75801fc465ebccfca08813ba5a9d65d
Charles Lesage <charles19lesage06@gmail.com>
2025-12-10 14:13:51 +0100

    Add initial Trap actor class and implementation
    Introduces the ATrap class derived from ABuilding, with basic constructor and BeginPlay override. This sets up the foundation for trap actors in the game.

---
+++ Source/DreadNight/Public/Actors/Traps/Trap.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Building.h"
+#include "Trap.generated.h"
+
+UCLASS()
+class DREADNIGHT_API ATrap : public ABuilding
+{
+	GENERATED_BODY()
+	
+public:
+	ATrap();
+
+protected:
+	virtual void BeginPlay() override;
+};
