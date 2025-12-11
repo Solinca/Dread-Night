d21659af15558936072095f417594f2f0885a9ff
SamyMENA-BOUR <61056787+SamyMENA-BOUR@users.noreply.github.com>
2025-12-11 16:53:10 +0100

    Add GlobalPopUp UI widget and implementation
    Introduces the GlobalPopUp widget for displaying popup messages in the UI. Includes header and source files with logic to set popup text, play an animation, and remove the widget after the animation finishes. Also updates related DayAndNight widget assets.

---
+++ Source/DreadNight/Private/UI/PopUp/GlobalPopUp.cpp
@@ -0,0 +1,31 @@
+#include "UI/PopUp/GlobalPopUp.h"
+
+#include "Components/TextBlock.h"
+
+
+void UGlobalPopUp::NativeConstruct()
+{
+	Super::NativeConstruct();
+}
+
+void UGlobalPopUp::SpawnPopUp(const FText& InText)
+{
+	if (!TopViewportText)
+	{
+		return;
+	}
+	
+	TopViewportText->SetText(InText);
+	
+	if (!ConstructAnimation)
+	{
+		return;
+	}
+	
+	PlayAnimation(ConstructAnimation);
+
+	FWidgetAnimationDynamicEvent AnimFinishedDelegate;
+	AnimFinishedDelegate.BindDynamic(this, &UUserWidget::RemoveFromParent);
+
+	BindToAnimationFinished(ConstructAnimation, AnimFinishedDelegate);
+}
