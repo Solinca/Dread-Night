8d647beb67cd02c75e49d93a5838f1fa6e3e3cc4
sharllesse <charles19lesage06@gmail.com>
2025-12-13 20:59:30 +0100

    Add base, attack, and range attack BT task nodes
    Introduces UBTTask_Base, UBTTask_Attack, and UBTTask_RangeAttack classes for AI behavior trees. These tasks handle melee and ranged attacks, including blackboard integration, memory management, and observer registration for dynamic AI behavior.

---
+++ Source/DreadNight/Private/IA/Tasks/BTTask_Attack.cpp
@@ -0,0 +1,138 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "IA/Tasks/BTTask_Attack.h"
+
+#include "AIController.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Float.h"
+#include "BehaviorTree/Blackboard/BlackboardKeyType_Object.h"
+#include "DamageSystem/Interface/Damageable.h"
+
+UBTTask_Attack::UBTTask_Attack()
+{
+	NodeName = "Attack";
+	
+	INIT_TASK_NODE_NOTIFY_FLAGS();
+
+	bTickIntervals = true;
+
+	AttackedTarget.AllowNoneAsValue(false);
+	AttackedTarget.AddObjectFilter(this, GET_MEMBER_NAME_CHECKED(UBTTask_Attack, AttackedTarget), AActor::StaticClass());
+}
+
+EBTNodeResult::Type UBTTask_Attack::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
+{
+	FBTAttackTaskMemory* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(NodeMemory)};
+	if (AttackTaskMemory->bInitialized)
+	{
+		return EBTNodeResult::Succeeded;
+	}
+
+	UBlackboardComponent* BlackboardComponent{OwnerComp.GetBlackboardComponent()};
+	
+	AttackTaskMemory->AttackCooldown = AttackCooldown.GetValue(OwnerComp);
+	AttackTaskMemory->AttackDamage = AttackDamage.GetValue(OwnerComp);
+	AttackTaskMemory->AttackedTarget = Cast<AActor>(BlackboardComponent->GetValue<UBlackboardKeyType_Object>(AttackedTarget.GetSelectedKeyID()));
+
+	if (!AttackTaskMemory->AttackedTarget.IsValid() ||
+		!AttackTaskMemory->AttackedTarget->Implements<UDamageable>())
+	{
+		return EBTNodeResult::Failed;
+	}
+	
+	RegisterToKeyIdChecked(AttackCooldown, OwnerComp, BlackboardComponent, this, &UBTTask_Attack::OnAttackCooldownKeyValueChange);
+	RegisterToKeyIdChecked(AttackDamage, OwnerComp, BlackboardComponent, this, &UBTTask_Attack::OnAttackDamageKeyValueChange);
+
+	BlackboardComponent->RegisterObserver(AttackedTarget.GetSelectedKeyID(), this,
+		FOnBlackboardChangeNotification::CreateUObject(this, &UBTTask_Attack::OnAttackedTargetKeyValueChange));
+
+	AAIController* AIController{OwnerComp.GetAIOwner()};
+	AIController->SetFocus(AttackTaskMemory->AttackedTarget.Get());
+	
+	AttackTaskMemory->bInitialized = true;
+	
+	return EBTNodeResult::InProgress;
+}
+
+void UBTTask_Attack::TickTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory, float DeltaSeconds)
+{
+	const auto* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(NodeMemory)};
+	if (!AttackTaskMemory->bInitialized)
+	{
+		FinishLatentAbort(OwnerComp);
+		return;
+	}
+
+	AActor* DamageInstigator{OwnerComp.GetAIOwner()->GetPawn()};
+	IDamageable* Damageable{Cast<IDamageable>(AttackTaskMemory->AttackedTarget.Get())};
+	
+	if (!Damageable || !DamageInstigator)
+	{
+		FinishLatentTask(OwnerComp, EBTNodeResult::Succeeded);
+		return;
+	}
+	
+	Damageable->TryApplyDamage(AttackTaskMemory->AttackDamage, DamageInstigator);
+	
+	SetNextTickTime(NodeMemory, AttackTaskMemory->AttackCooldown);
+}
+
+void UBTTask_Attack::OnTaskFinished(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory,
+									EBTNodeResult::Type TaskResult)
+{
+	auto* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(NodeMemory)};
+	AttackTaskMemory->bInitialized = false;
+	AttackTaskMemory->AttackedTarget.Reset();
+
+	UBlackboardComponent* BlackboardComponent{OwnerComp.GetBlackboardComponent()};
+	BlackboardComponent->UnregisterObserversFrom(this);
+
+	AAIController* AIController{OwnerComp.GetAIOwner()};
+	AIController->ClearFocus(EAIFocusPriority::LastFocusPriority);
+}
+
+void UBTTask_Attack::InitializeFromAsset(UBehaviorTree& Asset)
+{
+	Super::InitializeFromAsset(Asset);
+
+	if (const UBlackboardData* BBAsset = GetBlackboardAsset())
+	{
+		AttackedTarget.ResolveSelectedKey(*BBAsset);
+	}
+	else
+	{
+		AttackedTarget.InvalidateResolvedKey();
+	}
+}
+
+uint16 UBTTask_Attack::GetInstanceMemorySize() const
+{
+	return sizeof(FBTAttackTaskMemory);
+}
+
+EBlackboardNotificationResult UBTTask_Attack::OnAttackedTargetKeyValueChange(
+	const UBlackboardComponent& Blackboard, FBlackboard::FKey ChangedKeyID)
+{
+	auto* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(&Blackboard)};
+	AttackTaskMemory->AttackedTarget = Cast<AActor>(Blackboard.GetValue<UBlackboardKeyType_Object>(ChangedKeyID));
+	
+	return EBlackboardNotificationResult::ContinueObserving;
+}
+
+EBlackboardNotificationResult UBTTask_Attack::OnAttackCooldownKeyValueChange(
+	const UBlackboardComponent& Blackboard, FBlackboard::FKey ChangedKeyID)
+{
+	auto* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(&Blackboard)};
+	AttackTaskMemory->AttackCooldown = Blackboard.GetValue<UBlackboardKeyType_Float>(ChangedKeyID);
+	
+	return EBlackboardNotificationResult::ContinueObserving;
+}
+
+EBlackboardNotificationResult UBTTask_Attack::OnAttackDamageKeyValueChange(
+	const UBlackboardComponent& Blackboard, FBlackboard::FKey ChangedKeyID)
+{
+	auto* AttackTaskMemory{CastNodeMemory<FBTAttackTaskMemory>(&Blackboard)};
+	AttackTaskMemory->AttackDamage = Blackboard.GetValue<UBlackboardKeyType_Float>(ChangedKeyID);
+	
+	return EBlackboardNotificationResult::ContinueObserving;
+}
---
+++ Source/DreadNight/Public/IA/Tasks/BTTask_Base.h
@@ -0,0 +1,49 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+#include "BehaviorTree/ValueOrBBKey.h"
+#include "BTTask_Base.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class DREADNIGHT_API UBTTask_Base : public UBTTaskNode
+{
+	GENERATED_BODY()
+
+protected:
+	template<typename T>
+	T* CastNodeMemory(uint8* NodeMemory) const
+	{
+		return reinterpret_cast<T*>(NodeMemory);
+	}
+
+	template<typename T>
+	T* CastNodeMemory(const UBlackboardComponent* BlackboardComponent) const
+	{
+		UBehaviorTreeComponent* BehaviorTreeComponent{Cast<UBehaviorTreeComponent>(BlackboardComponent->GetBrainComponent())};
+		uint8* NodeMemory{BehaviorTreeComponent->GetNodeMemory(this, BehaviorTreeComponent->FindInstanceContainingNode(this))};
+		return reinterpret_cast<T*>(NodeMemory);
+	}
+
+	template<typename VBTy_, typename UOTy_, typename MFTy_>
+	requires(
+		std::derived_from<std::remove_cvref_t<VBTy_>, FValueOrBlackboardKeyBase> &&
+		std::derived_from<std::remove_pointer_t<UOTy_>, UObject> &&
+		std::is_member_function_pointer_v<MFTy_>
+	)
+	void RegisterToKeyIdChecked(VBTy_& ValueOrBlackboardKeyBase,
+		UBehaviorTreeComponent& BehaviorTreeComponent, UBlackboardComponent* BlackboardComponent, UOTy_* UserObject, MFTy_ MemberFunction)
+	{
+		const FBlackboard::FKey KeyId{ValueOrBlackboardKeyBase.GetKeyId(BehaviorTreeComponent)};
+		if (KeyId != FBlackboard::InvalidKey)
+		{
+			BlackboardComponent->RegisterObserver(KeyId, UserObject,
+				FOnBlackboardChangeNotification::CreateUObject(UserObject, MemberFunction));
+		}
+	}
+};
