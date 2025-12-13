a9dd185016f05df1a24e7891a1aee0f66e81b08f
Solinca <corbi.nicolas@gmail.com>
2025-12-13 18:24:32 +0100

    INTE-20 - Create a Torch Building

---
+++ Source/DreadNight/Private/Buildings/Torch.cpp
@@ -0,0 +1,17 @@
+#include "Buildings/Torch.h"
+#include "Subsystems/World/DayCycleSubSystem.h"
+#include "Components/PointLightComponent.h"
+
+ATorch::ATorch()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	Light = CreateDefaultSubobject<UPointLightComponent>("Light");
+
+	Light->SetupAttachment(MeshComp);
+}
+
+void ATorch::BeginPlay()
+{
+	Super::BeginPlay();
+}
