8ab04a9e3f474255e44fd733698a92a8c348d81c
Charles Lesage <95613353+sharllesse@users.noreply.github.com>
2025-12-14 12:46:46 +0100

    STRUCT-9 - Create a class trap (#114)

---
+++ Source/DreadNight/Private/Actors/Traps/Trap.cpp
@@ -0,0 +1,37 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Actors/Traps/Trap.h"
+
+ATrap::ATrap()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	TrapCollisionComponent = CreateDefaultSubobject<UBoxComponent>("Trap Collision Component");
+	TrapCollisionComponent->SetCollisionProfileName("Trap");
+	TrapCollisionComponent->SetupAttachment(RootComponent);
+}
+
+void ATrap::BeginPlay()
+{
+	Super::BeginPlay();
+
+	TrapCollisionComponent->OnComponentBeginOverlap.AddDynamic(this, &ATrap::OnBeginOverlap);
+}
+
+void ATrap::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	Super::EndPlay(EndPlayReason);
+
+	GetWorldTimerManager().ClearTimer(TickTimerHandle);
+}
+
+void ATrap::OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+							UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	if (ABaseAICharacter* BaseAICharacter{Cast<ABaseAICharacter>(OtherActor)})
+	{
+		SetLifeSpan(TrapLifeSpan);
+		GetWorldTimerManager().SetTimer(TickTimerHandle, this, &ATrap::OnTrapTick, PrimaryActorTick.TickInterval, true);
+	}
+}
---
+++ Source/DreadNight/Public/Actors/Traps/Trap.h
@@ -0,0 +1,42 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Actors/Building.h"
+#include "Components/BoxComponent.h"
+#include "IA/Characters/BaseAICharacter.h"
+#include "Trap.generated.h"
+
+UCLASS()
+class DREADNIGHT_API ATrap : public ABuilding
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Component")
+	TObjectPtr<UBoxComponent> TrapCollisionComponent;
+
+	/** Potential damage dealt to the monsters. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable")
+	float TrapDamage{0.f};
+
+	/** Life span set after the trap has been activated. */
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Variable")
+	float TrapLifeSpan{0.f};
+	
+private:
+	FTimerHandle TickTimerHandle;
+	
+public:
+	ATrap();
+
+protected:
+	virtual void BeginPlay() override;
+
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+
+	virtual void OnTrapTick() {}
+private:
+	UFUNCTION()
+	void OnBeginOverlap(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor,
+		UPrimitiveComponent* OtherComponent, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult);
+};
