be44b88f6adcd92958bf9a6df6741a49c16673ba
SamyMENA-BOUR <61056787+SamyMENA-BOUR@users.noreply.github.com>
2025-12-15 17:29:05 +0100

    GP-5 - Create a map | Create a Compass (#183)

---
+++ Source/DreadNight/Public/UI/Widgets/Map/MapWidget.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "MapWidget.generated.h"
+
+class UImage;
+
+
+UCLASS()
+class DREADNIGHT_API UMapWidget : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> MapImage;
+	
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<UImage> PlayerIcon;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<AActor> RenderTargetMap;
+
+private:
+	UPROPERTY(Transient)
+	TObjectPtr<APawn> OwnerPawn;
+
+	float RotationOffset{ 90.f };
+	float MapImageSize{ 900.f };
+	float WorldSize{ 30000.f };
+
+protected:
+	virtual void NativeConstruct() override;
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+};
