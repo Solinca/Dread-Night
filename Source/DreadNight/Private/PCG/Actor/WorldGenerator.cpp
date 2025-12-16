74cdc662edaeae2eabccb27405e980877a870c15
Nicolas Corbi <corbi.nicolas@gmail.com>
2025-12-16 12:02:37 +0100

    INTE-23 - Add Exclude zones and compute exclude zone rotation (#191)

---
+++ Source/DreadNight/Private/PCG/Actor/WorldGenerator.cpp
@@ -0,0 +1,80 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PCG/Actor/WorldGenerator.h"
+
+#include "Global/MyGameInstance.h"
+#include "Kismet/GameplayStatics.h"
+#include "PCG/Public/PCGVolume.h"
+#include "PCG/Public/PCGComponent.h"
+
+// Sets default values
+AWorldGenerator::AWorldGenerator()
+{
+	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void AWorldGenerator::BeginPlay()
+{
+	Super::BeginPlay();
+
+	for (auto Element : PCG_Array)
+	{
+		RegisterVolume(Element);
+	}
+	GenerateWorld();
+}
+
+void AWorldGenerator::RegisterVolume(APCGVolume* Volume)
+{
+	if (!Volume)
+		return;
+	Volume->PCGComponent->OnPCGGraphGeneratedDelegate.AddLambda([this](UPCGComponent*)
+	{
+		++PCGCurrentGenerationNumber;
+		if (PCGCurrentGenerationNumber >= PCGGenerationNumber)
+		{
+			if(UMyGameInstance* GameInstance = Cast<UMyGameInstance>(UGameplayStatics::GetGameInstance(this)))
+			{
+				GameInstance->OnPCGEndGeneration.Broadcast();				
+			}
+		}
+	});
+	++PCGGenerationNumber;
+}
+
+// Called every frame
+void AWorldGenerator::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+void AWorldGenerator::GenerateWorld()
+{
+	int Seed = FMath::RandRange(0, 999999);
+	for (APCGVolume* Volume : PCG_Array)
+	{
+		Volume->PCGComponent->Cleanup();
+		Volume->PCGComponent->Seed = Seed;
+		Volume->PCGComponent->Generate();
+	}
+}
+
+void AWorldGenerator::CleanUp()
+{
+	for (auto Element : PCG_Array)
+	{
+		Element->PCGComponent->Cleanup();
+	}
+}
+
+void AWorldGenerator::Generate()
+{
+	for (auto Element : PCG_Array)
+	{
+		Element->PCGComponent->Generate();
+	}
+}
+
---
+++ Source/DreadNight/Public/PCG/Actor/WorldGenerator.h
@@ -0,0 +1,46 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "WorldGenerator.generated.h"
+
+class APCGVolume;
+
+UCLASS()
+class DREADNIGHT_API AWorldGenerator : public AActor
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this actor's properties
+	AWorldGenerator();
+
+	UPROPERTY(EditAnywhere)
+	TArray<APCGVolume*> PCG_Array;
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	int PCGGenerationNumber;
+	int PCGCurrentGenerationNumber;
+
+
+	void RegisterVolume(APCGVolume* Volume);
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	void GenerateWorld();
+
+#if WITH_EDITOR
+
+	UFUNCTION(CallInEditor)
+	void CleanUp();
+	
+	UFUNCTION(CallInEditor)
+	void Generate();
+#endif
+	
+};
