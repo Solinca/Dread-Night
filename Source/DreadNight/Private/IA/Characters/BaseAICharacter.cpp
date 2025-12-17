fd58a8a3e56854f1e954ed3e7307e3fb179f4ca8
Chapix <lou.allerme@gmail.com>
2025-12-17 09:20:20 +0100

    Instance factory correction

--- Source/DreadNight/Private/IA/Characters/BaseAICharacter.cpp
+++ Source/DreadNight/Private/IA/Characters/BaseAICharacter.cpp
@@ -88,7 +88,7 @@ void ABaseAICharacter::DropLoot() const
 			int RandomStack = FMath::RandRange(LootData->MinDroppedAmount,LootData->MaxDroppedAmount);
 			UInventoryComponent* InventoryComp = GetWorld()->GetFirstPlayerController()->GetPawn()->GetComponentByClass<UInventoryComponent>();
 			if (InventoryComp != nullptr)
-				InventoryComp->AddItem(FItemInstanceFactory::CreateItem(InventoryComp->GetOwner(),LootData->ItemDataAsset,RandomStack));
+				InventoryComp->AddItem(UItemInstanceFactory::CreateItem(InventoryComp->GetOwner(),LootData->ItemDataAsset,RandomStack));
 		}
 	}
 }
