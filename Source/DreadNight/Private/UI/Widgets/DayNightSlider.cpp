2fc73f78bdbf1e97239bf72e89e270d3b78ae28b
SamyMENA-BOUR <61056787+SamyMENA-BOUR@users.noreply.github.com>
2025-12-19 19:09:28 +0100

    Add DayNightSlider widget and update player HUD
    Introduced the DayNightSlider UI widget with supporting C++ implementation to manage day/night cycle visualization. Updated player HUD to use new WBP_DayNightSlider and WBP_Hours assets, and removed the old WBP_Hours widget from the Misc folder.

---
+++ Source/DreadNight/Private/UI/Widgets/DayNightSlider.cpp
@@ -0,0 +1,48 @@
+﻿#include "UI/Widgets/DayNightSlider.h"
+
+#include "Components/Slider.h"
+#include "Global/BaseLevelWorldSettings.h"
+#include "Subsystems/World/DayCycleSubSystem.h"
+
+
+void UDayNightSlider::NativeConstruct()
+{
+	Super::NativeConstruct();
+
+	if (const ABaseLevelWorldSettings* BaseLevelWorldSettings = Cast<ABaseLevelWorldSettings>(GetWorld()->GetWorldSettings()))
+	{
+		DayNightSlider->SetMinValue(0.f);
+		DayNightSlider->SetMaxValue(1.f);
+		FullDayTime = BaseLevelWorldSettings->DayCycleSystemData->DayTimeInSeconds +
+			BaseLevelWorldSettings->DayCycleSystemData->DawnTimeInSeconds +
+			BaseLevelWorldSettings->DayCycleSystemData->DuskTimeInSeconds;
+	}
+
+	if (UDayCycleSubSystem* DayCycleSubSystem = GetWorld()->GetSubsystem<UDayCycleSubSystem>())
+	{
+		DayCycleSubSystem->OnDawnStart.AddDynamic(this, &ThisClass::StartDayCycle);
+		DayCycleSubSystem->OnNightStart.AddDynamic(this, &ThisClass::NightStart);
+	}
+}
+
+void UDayNightSlider::StartDayCycle()
+{
+	bIsDayCycle = true;
+	CurrentDayTime = 0.f;
+	DayNightSlider->SetValue(CurrentDayTime);
+}
+
+void UDayNightSlider::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	if (!bIsDayCycle)
+	{
+		return;
+	}
+	
+	Super::NativeTick(MyGeometry, InDeltaTime);
+
+	CurrentDayTime += InDeltaTime;
+
+	const float Time = CurrentDayTime / FullDayTime;
+	DayNightSlider->SetValue(Time);
+}
---
+++ Source/DreadNight/Public/UI/Widgets/DayNightSlider.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "DayNightSlider.generated.h"
+
+class USlider;
+
+
+UCLASS()
+class DREADNIGHT_API UDayNightSlider : public UUserWidget
+{
+	GENERATED_BODY()
+
+protected:
+	UPROPERTY(meta = (BindWidget))
+	TObjectPtr<USlider> DayNightSlider{ nullptr };
+
+private:
+	float CurrentDayTime{ 0.f };
+	float FullDayTime{ 0.f };
+	bool bIsDayCycle{ true };
+
+public:
+	virtual void NativeTick(const FGeometry& MyGeometry, float InDeltaTime) override;
+	
+protected:
+	virtual void NativeConstruct() override;
+
+private:
+	UFUNCTION()
+	void StartDayCycle();
+	UFUNCTION()
+	void NightStart() { bIsDayCycle = false; }
+	
+};
