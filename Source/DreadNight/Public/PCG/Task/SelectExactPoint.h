7a7573c1de36c67d098a54b3eb7a1fceca608298
Yann Grallan <116149587+Xanhos@users.noreply.github.com>
2026-01-10 20:48:49 +0100

    HOTFIX-63 - Fix PCG Generation (#454)

---
+++ Source/DreadNight/Public/PCG/Task/SelectExactPoint.h
@@ -0,0 +1,56 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PCGSettings.h"
+#include "SelectExactPoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS(BlueprintType, ClassGroup = (Procedural))
+class DREADNIGHT_API USelectExactPoint :  public UPCGSettings
+{
+	GENERATED_BODY()
+
+public:
+	USelectExactPoint();
+
+	//~Begin UPCGSettings interface
+#if WITH_EDITOR
+	virtual FName GetDefaultNodeName() const override { return FName(TEXT("Get All Road Data")); }
+	virtual FText GetDefaultNodeTitle() const override;
+	virtual FText GetNodeTooltipText() const override;
+	virtual EPCGSettingsType GetType() const override { return EPCGSettingsType::Spatial; }
+#endif
+
+protected:
+	virtual TArray<FPCGPinProperties> InputPinProperties() const override;
+	virtual TArray<FPCGPinProperties> OutputPinProperties() const override;
+	virtual FPCGElementPtr CreateElement() const override;
+	//~End UPCGSettings interface
+
+public: 
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings", meta = (PCG_Overridable))
+	int NumberOfPoint = 1;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings", meta = (PCG_Overridable))
+	int RandomSeed = 4198618;
+	
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Settings", meta = (PCG_Overridable))
+	bool bRandomSelection = false;
+};
+
+/**
+ * Custom PCG Element that executes the spline gathering logic
+ */
+class FUSelectExactPoint : public IPCGElement
+{
+protected:
+	virtual bool ExecuteInternal(FPCGContext* Context) const override;
+
+	virtual bool CanExecuteOnlyOnMainThread(FPCGContext* Context) const override;
+
+	virtual bool IsCacheable(const UPCGSettings* InSettings) const override;
+};
